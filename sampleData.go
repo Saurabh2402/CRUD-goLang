15e0cb6b30706d52daf0f53751a279d5f37ea524
Saurabh <saurabh.yelmame19@vit.edu>
2023-10-08 16:04:49 +0530

    Sample Data added for development

---
+++ sampleData.go
@@ -0,0 +1,98 @@
+package main
+
+var sampleBooks = []Book{
+	{
+		Id:   1,
+		Name: "SampleBook-1",
+		Authors: []Author{
+			{
+				FirstName: "John",
+				LastName:  "Doe",
+			},
+			{
+				FirstName: "Jane",
+				LastName:  "Smith",
+			},
+		},
+		PublishingInfo: Publisher{
+			Name: "Sample Publisher",
+			Year: 2023,
+		},
+	},
+
+	{
+		Id:   2,
+		Name: "SampleBook-2",
+		Authors: []Author{
+			{
+				FirstName: "John",
+				LastName:  "Doe",
+			},
+			{
+				FirstName: "Jane",
+				LastName:  "Smith",
+			},
+		},
+		PublishingInfo: Publisher{
+			Name: "Sample Publisher",
+			Year: 2023,
+		},
+	},
+
+	{
+		Id:   3,
+		Name: "SampleBook-3",
+		Authors: []Author{
+			{
+				FirstName: "John",
+				LastName:  "Doe",
+			},
+			{
+				FirstName: "Jane",
+				LastName:  "Smith",
+			},
+		},
+		PublishingInfo: Publisher{
+			Name: "Sample Publisher",
+			Year: 2023,
+		},
+	},
+
+	{
+		Id:   4,
+		Name: "SampleBook-4",
+		Authors: []Author{
+			{
+				FirstName: "John",
+				LastName:  "Doe",
+			},
+			{
+				FirstName: "Jane",
+				LastName:  "Smith",
+			},
+		},
+		PublishingInfo: Publisher{
+			Name: "Sample Publisher",
+			Year: 2023,
+		},
+	},
+
+	{
+		Id:   5,
+		Name: "SampleBook-5",
+		Authors: []Author{
+			{
+				FirstName: "John",
+				LastName:  "Doe",
+			},
+			{
+				FirstName: "Jane",
+				LastName:  "Smith",
+			},
+		},
+		PublishingInfo: Publisher{
+			Name: "Sample Publisher",
+			Year: 2023,
+		},
+	},
+}
